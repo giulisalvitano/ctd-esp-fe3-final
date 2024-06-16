28cda6f7d96994ff5fb0771dac2d65d03016bf05
giulisalvitano <salvitanogonzalezgiuliana@gmail.com>
2024-06-16 17:17:54 -0300

    Subiendo avances. Se logo el cambio de tema

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,20 @@ import { useDentistStates } from './utils/global.context'
 import { routes } from './utils/routes'
 
 const Navbar = () => {
-  const {dispatch} = useDentistStates()
+  const { state, dispatch } = useDentistStates();
+  const isDarkTheme = state.theme;
 
+  const handleThemeChange = () => {
+    dispatch({ type: 'CHANGE_THEME' });
+  };
+  
   return (
     <nav>
       <Link to={routes.home}><h4>Home</h4></Link>
       <Link to={routes.contact}><h4>Contacto</h4></Link>
       <Link to={routes.favs}><h4>Favs</h4></Link>
-      <button onClick={() => dispatch({ type: "CHANGE_THEME" })}>Change theme</button>
+      <button onClick={() => dispatch({ type: "CHANGE_THEME" })}>
+      {isDarkTheme ? '🌙' : '☀️'}</button>
     </nav>
   )
 }
